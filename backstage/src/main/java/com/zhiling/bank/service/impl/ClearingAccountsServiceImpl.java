d8ae92806427ed7e5e3f2de64b63e802c0c7d58a
Administrator <1650424511@qq.com>
2020-04-24 16:04:52 +0800

    朔哥

---
+++ backstage/src/main/java/com/zhiling/bank/service/impl/ClearingAccountsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zhiling.bank.service.impl;
+
+import com.zhiling.bank.service.ClearingAccountsService;
+
+import java.util.List;
+
+public class ClearingAccountsServiceImpl implements ClearingAccountsService {
+    @Override
+    public <T> List<T> read(String key) {
+        return null;
+    }
+}

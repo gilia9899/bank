343ef98f943b83377aade4c61f5400a30d5262dc
ryan-lau1 <1563065491@qq.com>
2020-05-06 10:28:32 +0800

    天气

---
+++ service-weather8018/src/main/java/com/zhiling/bank/ServiceWeatherMain8018.java
@@ -0,0 +1,15 @@
+package com.zhiling.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceWeatherMain8018 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWeatherMain8018.class,args);
+    }
+
+}

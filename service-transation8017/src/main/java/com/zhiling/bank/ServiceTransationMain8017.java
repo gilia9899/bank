368c82f18f41ce6c9d0641731afcf1f65ee8c943
ryan-lau1 <1563065491@qq.com>
2020-05-06 10:25:32 +0800

    刘新潮提交的历史交易记录

---
+++ service-transation8017/src/main/java/com/zhiling/bank/ServiceTransationMain8017.java
@@ -0,0 +1,14 @@
+package com.zhiling.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceTransationMain8017 {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTransationMain8017.class,args);
+    }
+
+}
